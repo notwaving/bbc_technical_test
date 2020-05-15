c6f2f27610fa7e3676b22531626b3621e1aca2cc
Samantha Burke <samanthaburke.uk@gmail.com>
2020-05-15 17:33:53 +0100

    function working and passing tests

--- app/word_reversal.js
+++ app/word_reversal.js
@@ -4,13 +4,13 @@
 // Write a function that can reverse words within a string, but NOT the order of the words
 function wordReversal(string) {
   // Strings are immutable so all changes need to be made into an array first.
-  // We're going to reverse everything in the string first, so we split every character to be treated as a separate array item
-  return string.split("");
+  // We're going to reverse everything in the string first, so we split every character to be treated as a separate array item. This also reverses the order of the words. If we repeat this process but split and join with *spaces*, we can revert the word order
+  return string.split("").reverse().join("").split(" ").reverse().join(" ");
 };
 
 
 // Reversing everything in the array will return a reversed order of words too.
-// Split this reversed stting by spaces, so we can isolate each word in an array, reverse each word and join() to return the result as a string to the user
+// Split this reversed string by spaces, so we can isolate each word in an array, reverse each word and join() to return the result as a string to the user
 
 // Call the function.
 console.log(wordReversal("friend or foe"));
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,9 +1,9 @@
 const utils = require('../app/word_reversal');
 
   describe("utils.wordReversal", () => {
-    test("returns an array when a string is passed to the function", () => {
-      expect(utils.wordReversal("friend")).toEqual([ 'f', 'r', 'i', 'e', 'n', 'd' ]);
-      expect(utils.wordReversal("friend or foe")).toEqual(["f", "r", "i", "e", "n", "d", " ", "o", "r", " ", "f", "o", "e"]);
+    test("reversed letters, but same word order when string is passed to the function", () => {
+      expect(utils.wordReversal("friend")).toBe("dneirf");
+      expect(utils.wordReversal("british broadcasting corporation")).toBe("hsitirb gnitsacdaorb noitaroproc");
     });
-
+  
   });
